1aee493c8f242cf04635cd01671e1f5fa948ae70
Mimoun <156517583+1Dh2Be@users.noreply.github.com>
2025-01-11 17:18:56 +0100

    Fix replies comment layout

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -31,34 +31,40 @@ export const Comment = ({
   };
 
   return (
-    <div className="h-64 rounded-xl bg-white mt-4 p-4 flex flex-col gap-3">
-      <div className="flex items-center gap-4">
-        <div className="w-10 h-10">
-          <img className="w-full h-full" src={avatar} alt="User avatar image" />
+    <div className="w-full flex flex-col items-end">
+      <div className="h-64 rounded-xl bg-white mt-4 p-4 flex flex-col gap-3 w-full">
+        <div className="flex items-center gap-4">
+          <div className="w-10 h-10">
+            <img
+              className="w-full h-full"
+              src={avatar}
+              alt="User avatar image"
+            />
+          </div>
+          <h2 className="text-neutral-darkBlue font-medium">{username}</h2>
+          <span className="text-neutral-grayishBlue">{date}</span>
         </div>
-        <h2 className="text-neutral-darkBlue font-medium">{username}</h2>
-        <span className="text-neutral-grayishBlue">{date}</span>
-      </div>
 
-      <p>{content}</p>
+        <p>{content}</p>
 
-      <div>
-        <div className="w-20 h-8 rounded-lg bg-primary-lightGrayishBlue/30 flex justify-between items-center px-2">
-          <FaPlus
-            onClick={increment}
-            className="text-primary-lightGrayishBlue"
-            size="12px"
-          />
-          <span className="text-primary-moderateBlue font-bold">{count}</span>
-          <FaMinus
-            onClick={decrement}
-            className="text-primary-lightGrayishBlue"
-            size="12px"
-          />
+        <div>
+          <div className="w-20 h-8 rounded-lg bg-primary-lightGrayishBlue/30 flex justify-between items-center px-2">
+            <FaPlus
+              onClick={increment}
+              className="text-primary-lightGrayishBlue"
+              size="12px"
+            />
+            <span className="text-primary-moderateBlue font-bold">{count}</span>
+            <FaMinus
+              onClick={decrement}
+              className="text-primary-lightGrayishBlue"
+              size="12px"
+            />
+          </div>
         </div>
       </div>
       {replies && replies.length > 0 && (
-        <div>
+        <div className="w-11/12">
           {replies.map((reply: any) => (
             <Comment
               avatar={reply.avatar}
